c820615a0bf1c6bb16e8cce3297f68675c4cb173
jeanneloise <144214273+jeanneloise@users.noreply.github.com>
2024-05-30 17:29:56 +0400

    Add files via upload

---
+++ Act_3mouse_interaction_2024_05_30_13_08_48/sketch.js
@@ -0,0 +1,18 @@
+
+
+var img;
+function preload() {
+img = loadImage("Image 12.jpg");
+}
+
+function setup () {
+createCanvas (500, 500);
+background(200);
+}
+
+function draw() {
+background(0);
+image(img, 0, 0);
+var v = map(mouseX, 0, width, 2, 20);
+filter(POSTERIZE, v);
+}
